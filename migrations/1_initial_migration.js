5062b76af29552901a2d5f78474b943b770b485f
DerekWebDev <dandre5950@yahoo.com>
2020-03-15 17:00:24 -0700

    First commit

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0x34D8361882d7250e7FB98728e27e35235D5bc32c',
+    '1000000000000000000000'
+  )
+};
